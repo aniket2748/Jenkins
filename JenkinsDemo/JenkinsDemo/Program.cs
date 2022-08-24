7a3d24b51a0f35ddda4b5e57ebe5f23ae8bbe509
Aniket Singh1 <aniketsingh.com@gmail.com>
2022-08-24 14:16:04 +0530

    First commit

--- JenkinsDemo/JenkinsDemo/Program.cs
+++ JenkinsDemo/JenkinsDemo/Program.cs
@@ -2,9 +2,13 @@
 {
     public class Program
     {
+        private string a;
         public static void Main()
         {
+            int a;
+            int b;
             Console.WriteLine("Hello World!");
         }
+
     }
 }
